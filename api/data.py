563b39afb56457c9e358728c8647fc31f99d11f8
Salim Hassouneh <salim.hassouneh@gmail.com>
2022-04-13 13:18:23 +0300

    fixing parsing

--- api/data.py
+++ api/data.py
@@ -22,6 +22,6 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write(message.encode)
+    self.wfile.write(message.encode())
     self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
     return
